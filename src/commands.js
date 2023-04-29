cf6b4889765502c6ae6b00a6c5c4c1f00e696e61
Sourov <SourovKH@users.noreply.github.com>
2023-04-29 17:42:23 +0530

    bash now can work with parent directory and argument

--- src/commands.js
+++ src/commands.js
@@ -7,18 +7,44 @@ const pwd = function(environment) {
 
 const ls = function(environment) {
   const {pwd} = environment;
-  const output = fs.readdirSync(pwd);
+  const output = fs.readdirSync(pwd).join("\t");
   return {pwd, output: [output]};
+}
+
+// cd arguments ==> .. , /, ../../something
+
+const resolvePath = function(path) {
+  const newPath = [];
+  const directories = path.split("/");
+
+  for(const currentDirectory of directories) {
+    if(currentDirectory === "..") {
+      newPath.pop();
+    }
+
+    if(currentDirectory !== ".."){
+      newPath.push(currentDirectory);
+    }
+  }
+
+  return newPath.join("/");
 }
 
 const cd = function(environment, arg) {
   let {pwd} = environment;
 
-  if(arg.includes(pwd)) {
+  if(arg.startsWith("/")) {
     return {pwd: arg, output: []};
   }
 
   pwd += `/${arg}`;
+
+  if(!fs.existsSync(pwd)) {
+    return {pwd, output: [`cd: no such file or directory: ${arg}`]};
+  }
+
+  pwd = resolvePath(pwd);
+
   return {pwd, output: []};
 }
 
